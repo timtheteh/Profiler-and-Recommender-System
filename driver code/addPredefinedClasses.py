cf24cc8e3cca4d38fe2c0119c208165db7cfddfd
timtheteh <76463517+timtheteh@users.noreply.github.com>
2023-07-11 02:42:34 -0400

    Update addPredefinedClasses.py

--- driver code/addPredefinedClasses.py
+++ driver code/addPredefinedClasses.py
@@ -5,10 +5,13 @@ import neo4j
 
 from fastchat.constants import LOGDIR
 
+### Databases ###
 dbms_username = "neo4j"
 dbms_password = "P@ssw0rd"
 graphDB = neo4j.GraphDatabase.driver("bolt://localhost:7687", auth=(f"{dbms_username}", f"{dbms_password}"), encrypted=False)
-
+### Models ###
+file_path = "/home/grace/grace/vicuna/FastChat/profiler/model.bin"
+sentence2vecModel = fasttext.load_model(file_path)
 ### Whitelist ###
 whitelist_file_path = "profiler/newEntityList2.txt"
 whitelist_filename = os.path.join(LOGDIR, f"{whitelist_file_path}")
@@ -17,14 +20,15 @@ with open(whitelist_filename, 'r') as f:
 whitelist = whitelist_string.split("\n")
 punctuationString = string.punctuation
 punctuationList=[*punctuationString]
-
 ### Locations List ###
 locations_file_path = "profiler/Locations.txt"
 locations_filename = os.path.join(LOGDIR, f"{locations_file_path}")
 with open(locations_filename, 'r') as f:
     locations_string = f.read()
-locations = whitelist_string.split("\n")
+locations = locations_string.split("\n")
 
+### Create predefined "Class" nodes based on whitelist ###
+# the first 70 whitelist terms will belong to Category 1, and so on 
 whiteListChunks = [whitelist[x:x+70] for x in range(0, len(whitelist), 70)]
 count = 1
 predefined_classes = []
@@ -34,17 +38,18 @@ for chunk in whiteListChunks:
     count+=1
 predefined_classes.append("Locations")
 
+### Create generic predefined "Class" nodes ###
+### Change this only ###
 generic_classes = ["Animals", "Air Force", "Navy", "Army", "Military Vehicles", "Global Politics", "South-East Asia"]
-
 predefined_classes+=generic_classes
 
+### Driver code ###
+# 1. collect the vector embedding for each class node
+# 2. run cypher qeury to create these class nodes
 class_vectors = {}
-file_path = "/home/grace/grace/vicuna/FastChat/profiler/model.bin"
-sentence2vecModel = fasttext.load_model(file_path)
 for class_node in predefined_classes:
     vector = sentence2vecModel.get_sentence_vector(class_node)
     class_vectors[class_node] = vector
-
 for class_node, vector in class_vectors.items():
     with graphDB.session() as session:
         session.run("""
