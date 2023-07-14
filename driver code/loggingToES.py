921ffea02ad0733bfdf58a43a7e1df763174b73f
timtheteh <76463517+timtheteh@users.noreply.github.com>
2023-07-14 05:09:55 -0400

    Update loggingToES.py

--- driver code/loggingToES.py
+++ driver code/loggingToES.py
@@ -15,7 +15,7 @@ def get_conv_log_filename():
 es_url = "https://localhost:9200"
 index="test2"
 username = "elastic"
-password = "e4uwp3kZm6maRubko8g0"
+password = "n*9jIwl3jw8ryhQhlmX0"
 data_list = []
 
 def bulk_index_log_data():
